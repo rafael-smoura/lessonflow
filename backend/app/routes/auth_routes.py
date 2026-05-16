bb2dc9f5b386536e151ca4628de53ec439034bd8
rafael-smoura <226293732+rafael-smoura@users.noreply.github.com.>
2026-05-15 23:35:42 -0300

    fix: correct Blueprint decorators and database imports

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -2,15 +2,15 @@ from flask import Blueprint, request, jsonify
 from app.models.user import User
 from app.extensions.extensions import db
 
-auth_print = Blueprint("auth",__name__)
+auth_bp = Blueprint("auth",__name__)
 
-@auth_print('/register', methods=["POST"])
+@auth_bp.route('/register', methods=["POST"])
 def register():
     data = request.get_json()
 
     username = data['username']
     email = data['email']
-    password = data["passowrd"]
+    password = data["password"]
 
     user = User(username=username, email=email)
     user.set_password(password)
