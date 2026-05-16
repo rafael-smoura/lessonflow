6e095c5207bbe2898b52179d4559d3a30c78f621
rafael-smoura <226293732+rafael-smoura@users.noreply.github.com.>
2026-05-16 00:42:25 -0300

    feat: implement complete lesson plan CRUD

--- backend/app/services/lesson_plan_service.py
+++ backend/app/services/lesson_plan_service.py
@@ -18,3 +18,35 @@ def create_lesson_plan(data):
 
 def get_all_lesson_plans():
     return LessonPlan.query.all()
+
+def get_lesson_plan_by_id(lesson_plan_id):
+    return LessonPlan.query.get(lesson_plan_id)
+
+def update_lesson_plan(lesson_plan_id, data):
+
+    lesson_plan = LessonPlan.query.get(lesson_plan_id)
+
+    if lesson_plan is None:
+        return None
+
+    lesson_plan.title = data["title"]
+    lesson_plan.objective = data["objective"]
+    lesson_plan.summary = data["summary"]
+    lesson_plan.subject = data["subject"]
+
+    db.session.commit()
+
+    return lesson_plan
+
+def delete_lesson_plan(lesson_plan_id):
+
+    lesson_plan = LessonPlan.query.get(lesson_plan_id)
+
+    if lesson_plan is None:
+        return None
+
+    db.session.delete(lesson_plan)
+
+    db.session.commit()
+
+    return lesson_plan
