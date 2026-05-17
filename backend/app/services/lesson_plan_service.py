6e455fa58b910c89c4a4b71759a513d6a8a7384c
rafael-smoura <226293732+rafael-smoura@users.noreply.github.com.>
2026-05-16 22:56:36 -0300

    feat: expand lesson plan model and improve API serialization

--- backend/app/services/lesson_plan_service.py
+++ backend/app/services/lesson_plan_service.py
@@ -1,13 +1,23 @@
+from datetime import datetime
+
 from app.extensions.extensions import db
 from app.models.lesson_plan import LessonPlan
 
 
 def create_lesson_plan(data):
+
     lesson_plan = LessonPlan(
         title=data["title"],
         objective=data["objective"],
         summary=data["summary"],
-        subject=data["subject"]
+        planned_date=datetime.strptime(
+            data["planned_date"],
+            "%Y-%m-%d"
+        ).date(),
+        discipline=data["discipline"],
+        contents=data["contents"],
+        support_resources=data["support_resources"],
+        tags=data["tags"]
     )
 
     db.session.add(lesson_plan)
@@ -16,11 +26,14 @@ def create_lesson_plan(data):
 
     return lesson_plan
 
+
 def get_all_lesson_plans():
     return LessonPlan.query.all()
 
+
 def get_lesson_plan_by_id(lesson_plan_id):
     return LessonPlan.query.get(lesson_plan_id)
+
 
 def update_lesson_plan(lesson_plan_id, data):
 
@@ -30,13 +43,28 @@ def update_lesson_plan(lesson_plan_id, data):
         return None
 
     lesson_plan.title = data["title"]
+
     lesson_plan.objective = data["objective"]
+
     lesson_plan.summary = data["summary"]
-    lesson_plan.subject = data["subject"]
+
+    lesson_plan.planned_date = datetime.strptime(
+        data["planned_date"],
+        "%Y-%m-%d"
+    ).date()
+
+    lesson_plan.discipline = data["discipline"]
+
+    lesson_plan.contents = data["contents"]
+
+    lesson_plan.support_resources = data["support_resources"]
+
+    lesson_plan.tags = data["tags"]
 
     db.session.commit()
 
     return lesson_plan
+
 
 def delete_lesson_plan(lesson_plan_id):
 
